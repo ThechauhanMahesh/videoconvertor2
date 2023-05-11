860763dcae3d18b134f881e3b8472b0585860b4b
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-11 10:49:08 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,16 +7,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", default=None)
-LOG_ID = config("LOG_ID", default=None)
-FORCESUB = config("FORCESUB", default=None)
-FORCESUB_UN = config("FORCESUB_UN", default=None)
-ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
-MONGODB_URI = config("MONGODB_URI", default=None)
-
+API_ID = 4796990
+API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
+BOT_TOKEN = "5031588187:AAHU2sPmghIzDfJULzR3kv9HNEP9tjOw2Yo"
+BOT_UN = "VideoConvertorRobot"
+FORCESUB = int("-1001581768849")
+FORCESUB_UN = "DroneBots"
+ACCESS_CHANNEL = int("-1001768362393")
+MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
+AUTH_USERS = 5351121397
+LOG_ID = 1844675200
+LOG_CHANNEL = "LogForVC"
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
