620ffc6f232f499a7f5ab3a8cf30ea0b8c8477c2
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-19 19:17:05 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 4796990
 API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
-BOT_TOKEN = "5031588187:AAHU2sPmghIzDfJULzR3kv9HNEP9tjOw2Yo"
+BOT_TOKEN = "5031588187:AAGf3XLCvPZH6DVtaEBpr-Dghv7lDNlRQOE"
 BOT_UN = "VideoConvertorRobot"
 FORCESUB = int("-1001581768849")
 FORCESUB_UN = "DroneBots"
