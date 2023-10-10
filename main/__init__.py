fad47232c90278dafbcb319c649b847fe647cd70
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-10-10 11:27:18 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -10,6 +10,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 2992000
 API_HASH = "235b12e862d71234ea222082052822fd"
 BOT_UN = "Batakk"
-BOT_TOKEN = "6669926453:AAEZc_j2UETA96mOgXTCWCmoi4RMfcdoycg"
+BOT_TOKEN = "6387408633:AAHq3eVrM0EqXt_aEeIWUQD2lwGlXlAEF1g"
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
